0088200f7e9bd1a5899721a3a09d2a62dc98cec6
ArtemGromyko <user@DESKTOP-7V56IJ5>
2021-10-06 12:34:04 +0300

    add dto

---
+++ SparePartsOrders.DTO/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SparePartsOrders.DTO
+{
+    public class OrderDto
+    {
+        public Guid RequestId { get; set; }
+        public string SparePartName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public bool Received { get; set; }
+    }
+}
